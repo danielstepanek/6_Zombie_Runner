1cb20912813a21278868e5909dc3d558dd3f0473
danielstepanek <56407814+danielstepanek@users.noreply.github.com>
2021-02-02 22:47:09 -0700

    174. Attack if provoked

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 100f;
+
+    NavMeshAgent navMeshAgent;
+    Transform destination;
+    float distanceToTarget = Mathf.Infinity;
+	bool isProvoked = false;
+
+
+    void Start()
+    {
+		navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+	{
+
+		CheckDistance();
+		if (isProvoked)
+		{
+			EngageTarget();
+		}
+	}
+
+	private void CheckDistance()
+	{
+		distanceToTarget = Vector3.Distance(gameObject.transform.position, target.position);
+
+		if (distanceToTarget <= chaseRange)
+		{
+			isProvoked = true;
+		}
+	}
+
+	private void EngageTarget()
+	{
+		if (distanceToTarget >= navMeshAgent.stoppingDistance)
+		{
+			navMeshAgent.SetDestination(target.position);
+		}
+		if (distanceToTarget <= navMeshAgent.stoppingDistance)
+		{
+			print("Attack!");
+
+		}
+	}
+
+	void OnDrawGizmosSelected()
+	{
+		// Display the explosion radius when selected
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, chaseRange);
+	}
+}
