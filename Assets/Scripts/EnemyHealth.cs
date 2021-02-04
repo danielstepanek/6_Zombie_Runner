2600e9ab179472de8a719394e2bfa412e0a0a2d4
danielstepanek <56407814+danielstepanek@users.noreply.github.com>
2021-02-03 22:57:51 -0700

    AI functionality, gun sfx.

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float hitPoints = 100f;
+
+    public void TakeDamage(float damage)
+	{
+        hitPoints -= damage;
+        if (hitPoints <= 0)
+		{
+			Destroy(gameObject);
+
+
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+	[SerializeField] Camera fpCamera;
+	[SerializeField] float raycastRange;
+	[SerializeField] float weaponDamage = 30f;
+	[SerializeField] AudioSource audioSource;
+	[SerializeField] AudioClip gunshot;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+		{
+            Shoot();
+		}
+    }
+
+	private void Shoot()
+	{
+		audioSource.PlayOneShot(gunshot);
+		RaycastHit hit;
+		if (Physics.Raycast(fpCamera.transform.position, fpCamera.transform.forward, out hit, raycastRange))
+		{
+			Debug.Log("I hit: " + hit.transform.name);
+
+			// TODO Add a hit effect
+
+			EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+			if(target == null) { return; }
+			else
+			{
+				target.TakeDamage(weaponDamage);
+			}
+
+		}
+		
+	}
+}
